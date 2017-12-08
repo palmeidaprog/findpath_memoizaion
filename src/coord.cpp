7ca0fb549193b40beff385889600cedf16987156
Paulo Almeida Filho <palmeidaprogramming@gmail.com>
2017-12-08 00:50:46 -0300

    [Sync] Creating Coord class

---
+++ src/coord.cpp
@@ -0,0 +1,13 @@
+/*
+ * @author Paulo R. Almeida Filho
+ * mail: palmeidaprogramming@gmail.com
+ * simple find path using memoization
+ * http://github.com/palmeidaprog/findpath_memoizaion
+ */
+
+
+#include "coord.h"
+
+const int &findpath::Coord::getPath() {
+
+}
---
+++ src/coord.h
@@ -0,0 +1,28 @@
+/*
+ * @author Paulo R. Almeida Filho
+ * mail: palmeidaprogramming@gmail.com
+ * simple find path using memoization
+ * http://github.com/palmeidaprog/findpath_memoizaion
+ */
+
+#ifndef _COORD_H_
+#define _COORD_H_
+
+namespace findpath {
+
+class Coord {
+    int const x;
+    int const y;
+    int &path;
+
+    const int &getPath();
+
+
+public:
+    Coord(int x, int y);
+
+};
+
+}
+
+#endif // _COORD_H_
